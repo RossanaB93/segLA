92c26baa7b7172f9fd7ca6157bb20a5435193aed
Rossana Buongiorno <74721555+RossanaB93@users.noreply.github.com>
2025-05-21 10:51:26 +0200

    Update read_and_write_cross_validation_scheme.py

--- utils/read_and_write_cross_validation_scheme.py
+++ utils/read_and_write_cross_validation_scheme.py
@@ -2,33 +2,25 @@ import os
 import pandas as pd
 
 cross_validation_scheme_path = r"/.../your_LAA_db.csv"    
-#cross_validation_scheme_path = r"/mnt/Dati2/Ilaria Network/CODICE PYTHON/dataset/3D_UNet_LAA_db.csv"    
 pat_folds = pd.read_csv(cross_validation_scheme_path, sep=",")
 folds = 5
-# Questo restituirà un DataFrame contenente solo le righe in cui la colonna fr"fold_{fold}"
-# ha il valore "Train"
-for fold in range(folds):
-    train_elements = pat_folds.loc[pat_folds[fr"Fold_{fold}"] == 'Train'].dropna()["Id"]
-    # Questo restituirà un DataFrame contenente solo le righe in cui la colonna "fold_0" 
-    # ha il valore "Validation"
-    val_elements = pat_folds.loc[pat_folds[fr"Fold_{fold}"] == 'Validation'].dropna()["Id"]
-    # Questo restituirà un DataFrame contenente solo le righe in cui la colonna "fold_0" 
-    # ha il valore "Test"
-    test_elements = pat_folds.loc[pat_folds[fr"Fold_{fold}"] == 'Test'].dropna()["Id"]
 
+for fold in range(folds):
+    # This will return a DataFrame containing only the rows where the column f"fold_{fold}" has the value "Train"
+    train_elements = pat_folds.loc[pat_folds[fr"Fold_{fold}"] == 'Train'].dropna()["Id"]
+    # This will return a DataFrame containing only the rows where the column f"fold_{fold}" has the value "Validation"
+    val_elements = pat_folds.loc[pat_folds[fr"Fold_{fold}"] == 'Validation'].dropna()["Id"]
+    
     os.chdir(r"/mnt/Dati2/Ilaria Network/CODICE PYTHON/balanced_dataset/")
     output_csv_file_tr = fr"train_elements_fold_{fold}.csv"
     output_csv_file_val = fr"val_elements_fold_{fold}.csv"
-    output_csv_file_test = fr"test_elements_fold_{fold}.csv"
+    output_csv_file_test = fr"test_elements.csv"
 
-    # Salva la serie come file CSV
+    # save as CSV file
     train_elements.to_csv(output_csv_file_tr, index=False)
     val_elements.to_csv(output_csv_file_val, index=False)
-    test_elements.to_csv(output_csv_file_test, index=False)
-
 
     print("File CSV salvato con successo.")
 
     del train_elements
     del val_elements
-    del test_elements
